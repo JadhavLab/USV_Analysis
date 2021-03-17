73fcd51b6fdbe430a3d465ea2326ca53326124b4
J Bladon <bladonjay@gmail.com>
2021-03-17 11:31:34 -0400

    created a couple files

---
+++ USV_procedure.m
@@ -0,0 +1,20 @@
+%% this is the beginning of the Procedure file
+
+%
+% produced by zach and jay
+%
+%
+%
+
+%% first we preprocess data
+
+edit preprocess_deepsqueak
+
+%%
+%%%%%%%%  now analyze data %%%%%%%%%%%%%
+%%
+
+edit calls_count_analysis
+
+
+%%
---
+++ preprocess_deepsqueak.m
@@ -0,0 +1,5 @@
+% preprocess the data
+%
+%
+%
+
