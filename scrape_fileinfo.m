2f046aa67d14a12cf6f0adb2c7e171cbbb339d92
zpranske <44886159+zpranske@users.noreply.github.com>
2021-03-17 17:33:06 -0400

    Pull subject ID and day tested from filenames

--- scrape_fileinfo.m
+++ scrape_fileinfo.m
@@ -4,9 +4,8 @@ function [pd,animal_id] = scrape_fileinfo(filename)
     
     %Ugly code to pull animal_id from file name
     %File name convention assumed to be e.g. 1_xf217-1BR Mar-16-2021  2_32 PM_Stats.xlsx
-    dashdelim = strfind(filename.name,'-');
-    spacedelim = strfind(filename.name,' ');
+    first = strfind(filename.name,'-')+1;
+    last = strfind(filename.name,' ')-1;
     fname = filename.name;
-    animal_id = fname(dashdelim(1)+1:spacedelim(1)-1);
-    animal_id = convertCharsToStrings(animal_id);
+    animal_id = str2num(convertCharsToStrings(fname(first:last)));
 end
