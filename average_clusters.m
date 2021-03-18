e0aebf296f3268b88b9444d77d6da614b5c9e399
zpranske <44886159+zpranske@users.noreply.github.com>
2021-03-18 10:01:51 -0400

    Updates

--- average_clusters.m
+++ average_clusters.m
@@ -6,10 +6,24 @@
 % Used to assess homeogeneity/validity of the clusters
 
 resize = [300 600];
+clusters = unique(T.Label);
 
+figure(1);
 
-indexes = ClusteringData{:,1}==;
-subT = T(indexes,:);
-
-
-new_images = imresize(ClusteringData{:,1},resize);
+for i=1:length(clusters)
+    %Resize all the images that correspond to label i
+    indexes = T.Label==i;
+    rows_orig = find(indexes==1);
+    rows = randsample(rows_orig,15); %Overlay a random subset of the images
+    composite_im = uint8([zeros(300) zeros(300)]);
+    for j=1:length(rows)
+        subTCell = ClusteringData{rows(j),1};
+        new_image = imresize(subTCell,resize);
+        ClusteringData{rows(j),1}=new_image;
+        composite_im = imfuse(composite_im,new_image);
+    end
+    subplot(4,6,i)
+    image(composite_im);
+    set(gca,'XTick',[], 'YTick', [])
+    box off;
+end
